87dfd250d40a1eff6fa74b2ade27f564ef7f25bc
Marti Casamayor Esteve <mcasamayor@GED.AD>
2026-03-13 12:45:09 +0100

    improved config file

--- configuration.py
+++ configuration.py
@@ -12,10 +12,9 @@ class FilterConfig:
     tag_key: str
     enabled: bool = True
 
-
+# # personal
 class Config:
     """Configuració centralitzada de columnes i visualització"""
-    
     
     # Columnes del fitxer d'estoc
     # POTS MODIFICAR NOM I PARÀMETRE
@@ -66,14 +65,15 @@ class Config:
     # Columnes a mostrar a la taula de components filtrats i les seves amplades
     # MODIFICA NOMÉS EL PARÀMETRE 
     COLUMNS_TO_SHOW = [ID, REFERENCE, PACKAGE, DESCRIPTION, MANUFACTURER_PN, MANUFACTURER_NAME]
-    COLUMNS_WIDTH = [50, 150, 80, 300, 150, 150]
+    COLUMNS_WIDTH = [None, 150, 80, 300, 150, 150]
 
 
     # Configuració de filtres
     # NO MODIFICAR
     FILTERS_CONFIG = [
         FilterConfig(
-            column_index=FILTRE1,
+            # column_index=FILTRE1,
+            column_index=locals().get("FILTRE1"),
             label_widget_name=  "filter1_label",
             line_edit_name=     "filter1_lineEdit",
             list_widget_name=   "filter1_listWidget",
@@ -82,7 +82,8 @@ class Config:
             enabled='FILTRE1' in locals()
         ),
         FilterConfig(
-            column_index=FILTRE2,
+            # column_index=FILTRE2,
+            column_index=locals().get("FILTRE2"),
             label_widget_name="filter2_label",
             line_edit_name="filter2_lineEdit",
             list_widget_name="filter2_listWidget",
@@ -92,7 +93,8 @@ class Config:
             enabled='FILTRE2' in locals()  # False perquè està comentat
         ),
         FilterConfig(
-            column_index=FILTRE3,
+            # column_index=FILTRE3,
+            column_index=locals().get("FILTRE3"),
             label_widget_name="filter3_label",
             line_edit_name="filter3_lineEdit",
             list_widget_name="filter3_listWidget",
@@ -102,7 +104,8 @@ class Config:
             enabled='FILTRE3' in locals()  # False perquè està comentat
         ),
         FilterConfig(
-            column_index=FILTRE4,
+            # column_index=FILTRE4,
+            column_index=locals().get("FILTRE4"),
             label_widget_name="filter4_label",
             line_edit_name="filter4_lineEdit",
             list_widget_name="filter4_listWidget",
@@ -112,7 +115,8 @@ class Config:
             enabled='FILTRE4' in locals()  # False perquè està comentat
         ),
         FilterConfig(
-            column_index=FILTRE5,
+            # column_index=FILTRE5,
+            column_index=locals().get("FILTRE5"),
             label_widget_name="filter5_label",
             line_edit_name="filter5_lineEdit",
             list_widget_name="filter5_listWidget",
@@ -130,13 +134,170 @@ class Config:
 
 
     # Google Sheet and credentials
-    # GOOGLE_SHEET_ID = "1jTJQL-PM7Uq1Gvns65RWnZOWWBl-359ZsKDoc6suoGQ"                # personal Stock22
-    # GOOGLE_CREDENTIALS_JSON = "credentials/mystock-482208-9ff7a94ae596.json"        # personal2
+    GOOGLE_SHEET_ID = "1jTJQL-PM7Uq1Gvns65RWnZOWWBl-359ZsKDoc6suoGQ"                # personal Stock22
+    GOOGLE_SHEET_GRID = "1061024957"
+    GOOGLE_CREDENTIALS_JSON = "credentials/mystock-482208-9ff7a94ae596.json"        # personal2
 
-    GOOGLE_SHEET_ID = "1cbyUW76l9EDPyHaKr98ARRroAWqfM3ctaYlRFw9enBg"              ## grupeina
-    GOOGLE_CREDENTIALS_JSON = "credentials/model-folio-482716-e4-ef9a987e1f30.json"        # personal2
+    # GOOGLE_SHEET_ID = "1cbyUW76l9EDPyHaKr98ARRroAWqfM3ctaYlRFw9enBg"              ## grupeina
+    # GOOGLE_CREDENTIALS_JSON = "credentials/model-folio-482716-e4-ef9a987e1f30.json"        # personal2
     
 
     # Timeout per peticions HTTP (segons)
     # MODIFICA NOMÉS EL PARÀMETRE 
     REQUEST_TIMEOUT = 10
+
+    # Visibilitat botons
+    COMPONENT_LOOPUP_BUTTON_VISIBILITY = True
+
+
+# grupeina
+# class Config:
+#     """Configuració centralitzada de columnes i visualització"""
+    
+    
+#     # Columnes del fitxer d'estoc
+#     # POTS MODIFICAR NOM I PARÀMETRE
+#     cantidad = 0
+#     id = 1
+#     marca_vehiculo = 2
+#     tipo_componente1 = 3
+#     marca_componente = 4
+#     familia_componente = 5
+#     tipo_componente2 = 6
+#     referencia_componente_1 = 7
+#     referencia_componente_2 = 8
+#     referencia_fabricante_1 = 9
+#     referencia_fabricante_2 = 10
+#     ubicacion = 11
+#     ubicación_nueva = 12
+#     estado = 13
+#     precio_de_compra = 14
+#     precio_de_venta = 15
+#     preveedor = 16
+#     link_de_compra = 17
+#     observaciones = 18
+#     datasheet = 19
+#     enmascarat = 20
+#     valor1 = 21
+#     valor2 = 22
+#     valor3 = 23
+#     encapsulat = 24
+#     trazabilidad = 25
+#     valor_total = 26
+
+
+
+
+#     # Paràmetres obligatoris per funcionar - columnes
+#     # MODIFICA NOMÉS EL PARÀMETRE
+#     ID = id                                 # número unic identificatiu de cada component
+#     STOCK = cantidad                           # estoc existent
+#     STORAGE = ubicación_nueva                       # localització del component
+#     WEB = datasheet                         # link d'una web, ja sigui del datasheet o la web on comprar o un tutorial
+#     REFERENCE = referencia_componente_1                 # nom del component
+    
+
+
+
+
+
+#     # Defineix els diferents filtres
+#     # MODIFICA NOMÉS EL PARÀMETRE, els que es deixin comentats es deshabiliten
+#     FILTRE1 = tipo_componente1
+#     FILTRE2 = marca_vehiculo
+#     FILTRE3 = tipo_componente2
+#     FILTRE4 = preveedor
+#     # FILTRE5 = MANUFACTURER_NAME
+
+#     TEXT_FILTER = referencia_componente_1           # columna que volem utilitzar el filtre de text
+
+
+
+
+    
+#     # Columnes a mostrar a la taula de components filtrats i les seves amplades
+#     # MODIFICA NOMÉS EL PARÀMETRE - ID va el primer obligatori
+#     COLUMNS_TO_SHOW = [id, referencia_componente_1, referencia_fabricante_1, familia_componente , estado, enmascarat, encapsulat]
+#     # COLUMNS_WIDTH = [50, 150, 150, 300, 80, 80]
+#     COLUMNS_WIDTH = [None, None, None, 300, None, None, None]
+
+
+#     # Configuració de filtres
+#     # NO MODIFICAR
+#     FILTERS_CONFIG = [
+#         FilterConfig(
+#             # column_index=FILTRE1,
+#             column_index=locals().get("FILTRE1"),
+#             label_widget_name=  "filter1_label",
+#             line_edit_name=     "filter1_lineEdit",
+#             list_widget_name=   "filter1_listWidget",
+#             button_name=        "filter1_pushButton",
+#             tag_key=            "filter1",
+#             enabled='FILTRE1' in locals()
+#         ),
+#         FilterConfig(
+#             # column_index=FILTRE2,
+#             column_index=locals().get("FILTRE2"),
+#             label_widget_name="filter2_label",
+#             line_edit_name="filter2_lineEdit",
+#             list_widget_name="filter2_listWidget",
+#             button_name="filter2_pushButton",
+#             tag_key="filter2",
+#             # enabled=True  # Canvia a False per desactivar
+#             enabled='FILTRE2' in locals()  # False perquè està comentat
+#         ),
+#         FilterConfig(
+#             # column_index=FILTRE3,
+#             column_index=locals().get("FILTRE3"),
+#             label_widget_name="filter3_label",
+#             line_edit_name="filter3_lineEdit",
+#             list_widget_name="filter3_listWidget",
+#             button_name="filter3_pushButton",
+#             tag_key="filter3",
+#             # enabled=True  # Canvia a False per desactivar
+#             enabled='FILTRE3' in locals()  # False perquè està comentat
+#         ),
+#         FilterConfig(
+#             # column_index=FILTRE4,
+#             column_index=locals().get("FILTRE4"),
+#             label_widget_name="filter4_label",
+#             line_edit_name="filter4_lineEdit",
+#             list_widget_name="filter4_listWidget",
+#             button_name="filter4_pushButton",
+#             tag_key="filter4",
+#             # enabled=False  # Canvia a False per desactivar
+#             enabled='FILTRE4' in locals()  # False perquè està comentat
+#         ),
+#         FilterConfig(
+#             # column_index=FILTRE5,
+#             column_index=locals().get("FILTRE5"),
+#             label_widget_name="filter5_label",
+#             line_edit_name="filter5_lineEdit",
+#             list_widget_name="filter5_listWidget",
+#             button_name="filter5_pushButton",
+#             tag_key="filter5",
+#             # enabled=False  # Canvia a False per desactivar
+#             enabled='FILTRE5' in locals()  # False perquè està comentat
+#         ),
+#     ]
+
+    
+#     # amaga o desavilita els paràmetre de filtre NO disponibles --> True: amaga; False: deshabilita
+#     # MODIFICA NOMÉS EL PARÀMETRE 
+#     FILTRER_AVAILABILITY = True     
+
+
+#     # Google Sheet and credentials
+#     # GOOGLE_SHEET_ID = "1jTJQL-PM7Uq1Gvns65RWnZOWWBl-359ZsKDoc6suoGQ"                # personal Stock22
+#     # GOOGLE_CREDENTIALS_JSON = "credentials/mystock-482208-9ff7a94ae596.json"        # personal2
+
+#     GOOGLE_SHEET_ID = "1cbyUW76l9EDPyHaKr98ARRroAWqfM3ctaYlRFw9enBg"                    # grupeina
+#     GOOGLE_SHEET_GRID = "1217665239"
+#     GOOGLE_CREDENTIALS_JSON = "credentials/model-folio-482716-e4-ef9a987e1f30.json"        # grupeina
+    
+
+#     # Timeout per peticions HTTP (segons)
+#     # MODIFICA NOMÉS EL PARÀMETRE 
+#     REQUEST_TIMEOUT = 10
+
+#     COMPONENT_LOOPUP_BUTTON_VISIBILITY = False
--- myStock.py
+++ myStock.py
@@ -26,182 +26,6 @@ from google.oauth2.service_account import Credentials
 import component_lookup
 from configuration import Config, FilterConfig
 
-# @dataclass
-# class FilterConfig:
-#     """Configuració d'un filtre"""
-#     column_index: int
-#     label_widget_name: str
-#     line_edit_name: str
-#     list_widget_name: str
-#     button_name: str
-#     tag_key: str
-#     enabled: bool = True
-
-
-# class Config:
-#     """Configuració centralitzada de columnes i visualització"""
-#     # Índexs de columnes - PARÀMETRES OBLIGATORIS
-#     ID = 0
-#     MANUFACTURER_PN = 1
-#     MANUFACTURER_NAME = 2
-#     CATEGORY = 3
-#     SUPPLIER = 4
-#     SUPPLIER_PN = 5
-#     SUPPLIER_CATEGORY = 6
-#     PACKAGE = 7
-#     DESCRIPTION = 8
-#     STOCK = 9
-#     STORAGE = 10
-#     DATASHEET = 11
-#     SUPPLIER_PRODUCT = 12
-
-
-
-
-#     ## FOR LABELS
-#     STOCK = STOCK                   # ES MOSTRE EN LABEL
-#     STORAGE = STORAGE                 # ES MOSTRE EN LABEL
-#     WEB = DATASHEET                     # S'OBRE LA WEB
-#     REFERENCE = SUPPLIER_PN               # PN o ID unic, PER identificar cada FILA
-#     TEXT_FILTER = MANUFACTURER_PN             # COLUMNA EN LA QUAL APLICAREM EL FILTRE DE TEXT
-
-
-
-
-
-#     # DEFINIR ELS FILTRES
-#     FILTRE1 = CATEGORY
-#     FILTRE2 = SUPPLIER_CATEGORY
-#     FILTRE3 = PACKAGE
-#     FILTRE4 = SUPPLIER
-#     FILTRE5 = MANUFACTURER_NAME
-
-
-
-
-
-
-#     # CANTIDAD = 0
-#     # ID = 1
-#     # MARCA_VEHICULO = 2
-#     # TIPO_COMPONENTE_1 = 3
-#     # MARCA_COMPONENTE = 4
-#     # FAMILIA_COMPONENTE = 5
-#     # TIPO_COMPONENTE_2 = 6
-#     # REFERENCIA_COMPONENTE_1 = 7
-#     # REFERENCIA_COMPONENTE = 8
-#     # REFERENCIA_FABRICANTE_1 = 9
-#     # REFERENCIA_FABRICANTE_2 = 10
-#     # UBICACION = 11
-#     # UBICACION_NUEVA = 12
-#     # ESTAD0 = 13
-#     # DATASHEET = 19
-
-
-
-#     # STOCK = CANTIDAD                   # ES MOSTRE EN LABEL
-#     # STORAGE = UBICACION                # ES MOSTRE EN LABEL
-#     # WEB = DATASHEET                     # S'OBRE LA WEB
-#     # REFERENCE = REFERENCIA_COMPONENTE_1               # PN o ID unic, PER identificar cada FILA
-#     # TEXT_FILTER = REFERENCIA_COMPONENTE_1             # COLUMNA EN LA QUAL APLICAREM EL FILTRE DE TEXT
-
-
-
-#     # # ALTRES  - PARÀMETRES OPCIONALS - NOMÉS SÓN PER UTILITZAR DINS DE LA FUNCIÓ CONFIG
-#     # # TYPE = 3                    # Tipo components
-#     # # DESCRIPTION = 5             # Familia components
-#     # # MARCA_COMPONENT = 4
-
-
-#     # # DEFINIR ELS FILTRES
-#     # FILTRE1 = MARCA_VEHICULO
-#     # FILTRE2 = TIPO_COMPONENTE_1
-#     # FILTRE3 = MARCA_COMPONENTE  
-#     # FILTRE4 = FAMILIA_COMPONENTE
-#     # FILTRE5 = TIPO_COMPONENTE_2
-
-    
-
-    
-#     # Columnes a mostrar a la taula i les seves amplades
-#     COLUMNS_TO_SHOW = [ID, REFERENCE, PACKAGE, DESCRIPTION, MANUFACTURER_PN, MANUFACTURER_NAME]
-#     COLUMNS_WIDTH = [50, 150, 80, 300, 150, 150]
-
-#     # # COLUMNS_TO_SHOW = [REFERENCIA_COMPONENTE_1, FAMILIA_COMPONENTE, REFERENCIA_FABRICANTE_1]
-#     # COLUMNS_WIDTH = [150, 400, 150]
-    
-#     # Configuració de filtres (pots activar/desactivar els que vulguis)
-#     FILTERS_CONFIG = [
-#         FilterConfig(
-#             column_index=FILTRE1,
-#             label_widget_name="filter1_label",
-#             line_edit_name="filter1_lineEdit",
-#             list_widget_name="filter1_listWidget",
-#             button_name="filter1_pushButton",
-#             tag_key="filter1",
-#             # enabled=True  # Canvia a False per desactivar
-#             enabled='FILTRE1' in locals()  # False perquè està comentat
-#         ),
-#         FilterConfig(
-#             column_index=FILTRE2,
-#             label_widget_name="filter2_label",
-#             line_edit_name="filter2_lineEdit",
-#             list_widget_name="filter2_listWidget",
-#             button_name="filter2_pushButton",
-#             tag_key="filter2",
-#             # enabled=True  # Canvia a False per desactivar
-#             enabled='FILTRE2' in locals()  # False perquè està comentat
-#         ),
-#         FilterConfig(
-#             column_index=FILTRE3,
-#             label_widget_name="filter3_label",
-#             line_edit_name="filter3_lineEdit",
-#             list_widget_name="filter3_listWidget",
-#             button_name="filter3_pushButton",
-#             tag_key="filter3",
-#             # enabled=True  # Canvia a False per desactivar
-#             enabled='FILTRE3' in locals()  # False perquè està comentat
-#         ),
-#         FilterConfig(
-#             column_index=FILTRE4,
-#             label_widget_name="filter4_label",
-#             line_edit_name="filter4_lineEdit",
-#             list_widget_name="filter4_listWidget",
-#             button_name="filter4_pushButton",
-#             tag_key="filter4",
-#             # enabled=False  # Canvia a False per desactivar
-#             enabled='FILTRE4' in locals()  # False perquè està comentat
-#         ),
-#         FilterConfig(
-#             column_index=FILTRE5,
-#             label_widget_name="filter5_label",
-#             line_edit_name="filter5_lineEdit",
-#             list_widget_name="filter5_listWidget",
-#             button_name="filter5_pushButton",
-#             tag_key="filter5",
-#             # enabled=False  # Canvia a False per desactivar
-#             enabled='FILTRE5' in locals()  # False perquè està comentat
-#         ),
-#     ]
-
-#     # self.ui.listWidget_4.setVisible(False)
-    
-    
-#     FILTRER_AVAILABILITY = True     # amaga o desavilita els paràmetre de filtre NO disponibles --> True: amaga; False: desavilita
-
-#     # Google Sheet ID
-#     # GOOGLE_SHEET_ID = "1U3H3R8ggRW-nEao_R1RXQ-l8WJdiGkXbWTSRkL0peRA"                # personal Stock
-#     GOOGLE_SHEET_ID = "1jTJQL-PM7Uq1Gvns65RWnZOWWBl-359ZsKDoc6suoGQ"                # personal Stock22
-#     # GOOGLE_SHEET_ID = "1cbyUW76l9EDPyHaKr98ARRroAWqfM3ctaYlRFw9enBg"              ## grupeina 
-#     # GOOGLE_SHEET_ID = "13rzlU99m8AtWtMIPll6iB_1iJge1rUPmn_03aEMuvQk"              # Còpia multiplicada stock
-
-
-#     # GOOGLE_CREDENTIALS_JSON = "credentials/mystock-482208-a553ed840217.json"        # personal
-#     GOOGLE_CREDENTIALS_JSON = "credentials/mystock-482208-9ff7a94ae596.json"        # personal2
-#     # GOOGLE_CREDENTIALS_JSON = "credentials/model-folio-482716-e4-e9eb4cc77d58.json" ## grupeina 
-    
-#     # Timeout per peticions HTTP (segons)
-#     REQUEST_TIMEOUT = 10
 
 
 class TagWidget(QWidget):
@@ -387,7 +211,11 @@ class MainWindow(QMainWindow):
         # Botons d'acció
         self.ui.pushButton_4.clicked.connect(self._open_google_sheet)
         self.ui.pushButton_5.clicked.connect(self._refresh_data)
-        self.ui.pushButton_6.clicked.connect(self._open_lookup)
+        if Config.COMPONENT_LOOPUP_BUTTON_VISIBILITY:
+            self.ui.pushButton_6.clicked.connect(self._open_lookup)
+            self.ui.pushButton_6.setVisible(True)
+        else:
+            self.ui.pushButton_6.setVisible(False)
         self.ui.datasheetButton.clicked.connect(self._open_datasheet)
         
         # Botó per esborrar tots els filtres
@@ -878,7 +706,8 @@ class MainWindow(QMainWindow):
         
         if hasattr(Config, "COLUMNS_WIDTH"):
             for idx, width in enumerate(Config.COLUMNS_WIDTH):
-                self.ui.tableWidget.setColumnWidth(idx, width)
+                if width is not None:
+                    self.ui.tableWidget.setColumnWidth(idx, width)
         else:
             self.ui.tableWidget.resizeColumnsToContents()
         
@@ -1029,7 +858,8 @@ class MainWindow(QMainWindow):
     def _open_google_sheet(self):
         """Obre el Google Sheet en una nova pestanya a la fila del component seleccionat"""
         base_url = f"https://docs.google.com/spreadsheets/d/{Config.GOOGLE_SHEET_ID}"
-        gid = "1061024957"  # ID de la pestanya correcta
+        # gid = "1061024957"  # ID de la pestanya correcta
+        gid = Config.GOOGLE_SHEET_GRID
         
         # Intentar obtenir el component seleccionat
         row_index = self.ui.tableWidget.currentRow()
